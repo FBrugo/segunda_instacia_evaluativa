a9c3f8f8663b04476c38fa31431f1ee0079b1f40
Ravila02 <avilaricardo1985@gmail.com>
2025-11-09 16:48:17 -0300

    Message: Se modifico el archivo Reporte y Juagadores

--- Segunda_Instancia_Evaluativa/src/segunda_instancia_evaluativa/persistencia/ArchivoJugadoresDAO.java
+++ Segunda_Instancia_Evaluativa/src/segunda_instancia_evaluativa/persistencia/ArchivoJugadoresDAO.java
@@ -4,10 +4,23 @@
  */
 package segunda_instancia_evaluativa.persistencia;
 
-/**
- *
- * @author fedee
- */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import segunda_instancia_evaluativa.modelo.Jugador;
+
 public class ArchivoJugadoresDAO {
-    
+    public static void guardarJugadores(List<Jugador> jugadores) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("jugadores.txt", false))) {
+            for (Jugador j : jugadores) {
+                bw.write(j.getNombre() + ";" + j.getApodo() + ";" + j.getTipo() + ";" +
+                         j.getDinero() + ";" + j.getVictorias());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error guardando jugadores: " + e.getMessage());
+        }
+    }
+
 }
